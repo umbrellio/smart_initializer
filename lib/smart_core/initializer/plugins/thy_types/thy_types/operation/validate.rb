33ded82f5505144955ebe59a0224ca8f3a104e16
basherru <alexander.baz@okwork.io>
2021-02-04 13:28:43 +0300

    dev

--- lib/smart_core/initializer/plugins/thy_types/thy_types/operation/validate.rb
+++ lib/smart_core/initializer/plugins/thy_types/thy_types/operation/validate.rb
@@ -13,7 +13,7 @@ module SmartCore::Initializer::TypeSystem::ThyTypes::Operation
       return if type.check(value).success?
 
       raise(SmartCore::Initializer::ThyTypeValidationError, <<~ERROR_MESSAGE)
-        Incorrect type of #{context.attribute.name} attribute for #{context.instance.class}
+        Incorrect type of #{context.attribute.name.inspect} attribute for #{context.instance.class}
         Expected: #{type.inspect}, but got: #{value.inspect}
       ERROR_MESSAGE
     end
